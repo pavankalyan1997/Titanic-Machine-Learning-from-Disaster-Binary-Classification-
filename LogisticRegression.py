a48d083c259b0a9bb8268904da642ca155f3407e
purandur <purandur@PURANDUR-LAP.oradev.oraclecorp.com>
2018-10-20 22:49:22 +0530

    Second commit

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt;
 import pandas as pd;
 
 class LogisticRegression:
-    def __init__(self,X,Y):
+    def __init__(self,X,Y,X_val,Y_val,X_test):
         #ones=np.ones(X.shape[0])
         #X=np.c_[ones,X]
         self.X=X
         self.Y=Y
+        self.X_val=X_val
+        self.Y_val=Y_val
+        self.X_test=X_test
         self.m=X.shape[0]
         self.n=X.shape[1]
         self.theta=np.random.randn(self.X.shape[1])
@@ -16,22 +19,39 @@ class LogisticRegression:
     def sigmoid(self,z):
         return 1/(1+np.exp(-z))
         
-    def computeCostFunction(self):
+    def computeCostFunction(self,Lambda):
         h=self.sigmoid(np.matmul(self.X,self.theta))
         self.J=(-1/self.m)*np.sum(self.Y*np.log(h)+(1-self.Y)*np.log(1-h))
+        temp=self.theta
+        temp[0]=0
+        Reg=Lambda*np.sum(self.theta**2)
+        self.J+=Reg
         return self.J
     
-    def performGradientDescent(self,num_of_iter,alpha):
+    def performGradientDescent(self,num_of_iter,alpha,Lambda):
         self.Cost_history=[]
         self.theta_history=[]
+        self.TrainingError=[]
+        self.ValError=[]
         for x in range(num_of_iter):
             h=self.sigmoid(np.matmul(self.X,self.theta))
-            J=self.computeCostFunction()
+            hVal=self.sigmoid(np.matmul(self.X_val,self.theta))
+            trainError=self.computeError(h,self.Y)
+            valError=self.computeError(hVal,self.Y_val)
+            self.TrainingError.append(trainError)
+            self.ValError.append(valError)
+            J=self.computeCostFunction(Lambda)
             self.Cost_history.append(J)
+            RegTheta=self.theta
+            RegTheta[0]=0
             self.theta_history.append(self.theta)
             temp=h-self.Y
-            self.theta=self.theta-(alpha/self.m)*(self.X.T.dot(temp))
-        return self.theta,self.Cost_history,self.theta_history
+            self.theta=self.theta-(alpha/self.m)*(self.X.T.dot(temp))-((alpha*Lambda)/self.m)*RegTheta
+        return self.theta,self.Cost_history,self.theta_history,self.TrainingError,self.ValError
+    
+    def computeError(self,h,y):
+        m=len(y)
+        return (1/2/m)*np.sum((h-y)**2)
             
         
     def predict(self,X_test):
--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 traindataset=pd.read_csv('newtrain.csv')
-X_train=traindataset.iloc[:,1:8].values
-Y_train=traindataset.iloc[:,0:1].values.flatten()
+#Rrandomly shuffle data
+from sklearn.utils import shuffle
+traindataset=shuffle(traindataset)
+trainsetsize=int(traindataset.shape[0]*0.8)
+X_train=traindataset.iloc[:trainsetsize,1:8].values
+Y_train=traindataset.iloc[:trainsetsize,0:1].values.flatten()
+
+X_val=traindataset.iloc[trainsetsize:,1:8].values
+Y_val=traindataset.iloc[trainsetsize:,0:1].values.flatten()
 
 testDataset=pd.read_csv('newtest.csv')
 X_test=testDataset.iloc[:,:].values
@@ -12,9 +19,13 @@ X_test=testDataset.iloc[:,:].values
 
 #Taking care of missing values
 from sklearn.preprocessing import Imputer
-imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)
-imputer=imputer.fit(X_train[:,2:3])
-X_train[:,2:3]=imputer.transform(X_train[:,2:3])
+imputerTrain=Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputerTrain=imputerTrain.fit(X_train[:,2:3])
+X_train[:,2:3]=imputerTrain.transform(X_train[:,2:3])
+
+imputerVal=Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputerVal=imputerVal.fit(X_val[:,2:3])
+X_val[:,2:3]=imputerVal.transform(X_val[:,2:3])
 
 imputerTest=Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputerTest=imputerTest.fit(X_test[:,2:3])
@@ -27,11 +38,8 @@ X_test[:,5:6]=imputerTest.transform(X_test[:,5:6])
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 labelencoder1=LabelEncoder()
 X_train[:,1]=labelencoder1.fit_transform(X_train[:,1])
+X_val[:,1]=labelencoder1.fit_transform(X_val[:,1])
 X_test[:,1]=labelencoder1.fit_transform(X_test[:,1])
-
-
-
-
 
 def encodeEmbarked(Embarked_dict,X):
     i=0
@@ -46,51 +54,56 @@ def encodeEmbarked(Embarked_dict,X):
         i+=1
     return Embarked_dict,X
 
-
 Embarked_dict={}
 Embarked_dict,X_train=encodeEmbarked(Embarked_dict,X_train)
+Embarked_dict,X_val=encodeEmbarked(Embarked_dict,X_val)
 Embarked_dict,X_test=encodeEmbarked(Embarked_dict,X_test)
 
-            
 imputer1=Imputer(missing_values='NaN',strategy='most_frequent',axis=0)
 imputer1=imputer1.fit(X_train[:,6:7])
 X_train[:,6:7]=imputer1.transform(X_train[:,6:7])
+X_val[:,6:7]=imputer1.transform(X_val[:,6:7])
 X_test[:,6:7]=imputer1.transform(X_test[:,6:7])
 
 X_train = np.array(list(X_train[:,:]), dtype=np.float)
+X_val = np.array(list(X_val[:,:]), dtype=np.float)
 X_test = np.array(list(X_test[:,:]), dtype=np.float)
-
 
 from sklearn.preprocessing import MinMaxScaler
 scaler=MinMaxScaler()
 scaler.fit(X_train)
 X_train=scaler.transform(X_train)
 scaler1=MinMaxScaler()
-scaler1.fit(X_test)
-X_test=scaler1.transform(X_test)
-
+scaler1.fit(X_val)
+X_val=scaler1.transform(X_val)
+scaler2=MinMaxScaler()
+scaler2.fit(X_test)
+X_test=scaler2.transform(X_test)
 
 from sklearn.preprocessing import PolynomialFeatures
 poly1=PolynomialFeatures(2)
 X_train=poly1.fit_transform(X_train)
 poly2=PolynomialFeatures(2)
 X_test=poly2.fit_transform(X_test)
-
-
+poly3=PolynomialFeatures(2)
+X_val=poly3.fit_transform(X_val)
 
 from LogisticRegression import LogisticRegression
-classifier=LogisticRegression(X_train,Y_train)
-J=classifier.computeCostFunction()
+classifier=LogisticRegression(X_train,Y_train,X_val,Y_val,X_test)
+initialJ=classifier.computeCostFunction(0.01)
 theta=classifier.returnTheta()
-theta,Cost_history,theta_history=classifier.performGradientDescent(100000,0.01)
-J=classifier.computeCostFunction()
+theta,Cost_history,theta_history,TrainingError,ValError=classifier.performGradientDescent(1000000,0.01,0.1)
 Y_pred=classifier.predict(X_test)
 
+plt.plot(TrainingError)
+plt.title('Learning Curves')
+plt.xlabel('Number of iterations')
+plt.ylabel('Error')
+plt.plot(ValError,'r')
 
 solution=pd.read_csv('kaggle.csv') 
 y_test=solution.iloc[:,1:2].values.flatten()
 
-   
 errorPercentage=(sum(abs(Y_pred-y_test)))/(len(Y_pred))*100
 accuracy=100-errorPercentage
 
@@ -106,7 +119,3 @@ with open('solution.csv','w',newline='') as csvFile:
     writer.writerows(solutionData)
 csvFile.close()
 
-
-
-    
-        
